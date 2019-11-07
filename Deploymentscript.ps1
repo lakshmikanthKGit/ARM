a1d2e3c42975f81d8fe4734d5d074ddbcbe176ed
lakshmikanth K <laxmikanthforu@yahoo.com>
2019-11-07 12:29:07 +0100

    adding deployment script

---
+++ Deploymentscript.ps1
@@ -0,0 +1,3 @@
+New-AzResourceGroupDeployment -Name test -ResourceGroupName MunichTest_lakshmi -TemplateFile "C:\VSCODE\ARM-Repo\ARM\Practice\Multiplevms\Multiplevm.json" -TemplateParameterFile "C:\VSCODE\ARM-Repo\ARM\Practice\Multiplevms\multiplevmparm.json"
+
+
