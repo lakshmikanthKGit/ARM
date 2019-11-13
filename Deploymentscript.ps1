5a342993bb88bd22ed2b22b7eeda03d0eed2c0aa
lakshmikanth K <laxmikanthforu@yahoo.com>
2019-11-13 14:25:58 +0100

    changes made

--- Deploymentscript.ps1
+++ Deploymentscript.ps1
@@ -1,3 +1,3 @@
-New-AzResourceGroupDeployment -Name test -ResourceGroupName MunichTest_lakshmi -TemplateFile "C:\VSCODE\ARM-Repo\ARM\Practice\Multiplevms\Multiplevm.json" -TemplateParameterFile "C:\VSCODE\ARM-Repo\ARM\Practice\Multiplevms\multiplevmparm.json"
+New-AzResourceGroupDeployment -Name test -ResourceGroupName MunichTest_lakshmi -TemplateFile C:\VSCODE\ARM-Repo\ARM\Practice\MultiplevmsinLB\MultiplevminLB.json -TemplateParameterFile C:\VSCODE\ARM-Repo\ARM\Practice\MultiplevmsinLB\multiplevminLBparm.json
 
 
---
+++ Practice/MultiplevmsinLB/readme.md
@@ -0,0 +1,1 @@
+This template will allow you deploy multiple vms in single deployment and it will join you to existing network and subnet. vm will be created with managed disk.
